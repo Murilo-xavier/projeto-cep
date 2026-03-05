fd6c0fba1613b940a57c7560b022ebdb743dd0e5
Murilo Xavier <muriloxmacedo@gmail.com>
2026-03-05 10:18:09 -0300

    Add error handling to ViaCepService and update test class

--- src/principal/testes.java
+++ src/principal/testes.java
@@ -1,18 +1,12 @@
 package principal;
 
-import java.io.IOException;
-
 import servicos.ViaCepService;
 
 public class testes {
 
-    public static void main(String[] args) {
-        ViaCepService viaCepService = new ViaCepService();
-        try {
-            String endereco = viaCepService.buscarEndereco("01001000");
-            System.out.println(endereco);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args){
+       ViaCepService viaCepService = new ViaCepService();
+       String resposta = viaCepService.buscarEndereco("99999999");
+         System.out.println(resposta);
     }
 }   
--- src/servicos/ViaCepService.java
+++ src/servicos/ViaCepService.java
@@ -10,21 +10,31 @@ public class ViaCepService {
 
     private String URL_BASE = "https://viacep.com.br/ws/";
 
-    private String formatarURL(String cep){
+    private String formatarURL(String cep) {
         return URL_BASE + cep + "/json/";
     }
 
-    private HttpResponse<String> buscarEnderecoAPI(String cep) throws IOException, InterruptedException{
+    private HttpResponse<String> buscarEnderecoAPI(String cep) {
         String url = formatarURL(cep);
-        
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response;
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response;
+        } catch (IOException | InterruptedException e) {
+            System.out.print(e.getMessage());
+        }
+        return null;
     }
 
-    public String buscarEndereco(String cep) throws IOException, InterruptedException{
-        HttpResponse<String> response = buscarEnderecoAPI(cep);
-        return response.body();
+    public String buscarEndereco(String cep){
+        try{
+            HttpResponse<String> response = buscarEnderecoAPI(cep);
+            return response.body();
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+        return null;
     }
 }
