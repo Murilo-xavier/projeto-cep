357242d0db127e5ca6f4d12b8be405b7eda82030
Murilo Xavier <muriloxmacedo@gmail.com>
2026-03-05 10:28:59 -0300

    Add InteracaoUsuario class for user interaction and update testes to use it

--- src/principal/testes.java
+++ src/principal/testes.java
@@ -1,12 +1,15 @@
 package principal;
 
+import servicos.InteracaoUsuario;
 import servicos.ViaCepService;
 
 public class testes {
 
     public static void main(String[] args){
        ViaCepService viaCepService = new ViaCepService();
-       String resposta = viaCepService.buscarEndereco("99999999");
+       InteracaoUsuario.solicitarCep();
+       
+       String resposta = viaCepService.buscarEndereco(InteracaoUsuario.getCep());
          System.out.println(resposta);
     }
 }   
---
+++ src/servicos/InteracaoUsuario.java
@@ -0,0 +1,26 @@
+package servicos;
+
+import java.util.Scanner;
+
+public class InteracaoUsuario {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String cep;
+
+    private static boolean validarCep(String cep) {
+        return cep.matches("\\d{8}");
+    }
+    public static void solicitarCep() {
+        String resposta;
+        System.out.println("Digite o CEP que deseja consultar:");
+        resposta = scanner.nextLine();
+        if (validarCep(resposta)) {
+            cep = resposta;
+        } else {
+            System.out.println("CEP inválido!");
+        }
+    }
+
+    public static String getCep() {
+        return cep;
+    }
+}
