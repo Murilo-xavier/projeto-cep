822bc00fb9066fb1e10584d5c355980cbc978a5e
Murilo Xavier <muriloxmacedo@gmail.com>
2026-03-09 17:07:43 -0300

    Add GeradorArquivo class for file generation functionality

---
+++ src/servicos/GeradorArquivo.java
@@ -0,0 +1,22 @@
+package servicos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+
+
+public class GeradorArquivo {
+
+    public static void gerarArquivo(String nomeArquivo, List<String> conteudo) {
+        
+        try {
+            FileWriter file = new FileWriter(nomeArquivo);
+            String json = Conversor.converterObjetoParaJson(conteudo);
+            file.write(json);
+            file.close();
+        } catch (IOException e) {
+            System.out.println("--Algum erro ocorreu ao gerar o arquivo: " + e.getMessage());
+        }
+    }
+}
